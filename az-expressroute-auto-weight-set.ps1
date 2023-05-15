9bf28c0e8a35d19827761b5a96554b2f689dcb57
Yahaga <59613584+pzohaycuoi@users.noreply.github.com>
2023-05-15 21:32:14 +0700

    Update az-expressroute-auto-weight-set.ps1

--- az-expressroute-auto-weight-set.ps1
+++ az-expressroute-auto-weight-set.ps1
@@ -20,16 +20,21 @@ param (
 # Ping to the on-prem IP address in a loop with interval of 5 seconds
 # If ping is not successful set the Routing weight of secondary connection to 30 and primary connection to 20
 
-Connect-AzAccount
-
 while ($true) {
   # Ping the on-prem IP address
   $ping = Test-NetConnection $OnpremIpAddress -Count 5
   if ($ping -eq $false) {
     # If ping is not successful
+    # Connect to Azure
+    $AzureTenantId= "<tenantid>"
+    $AzureSubscriptionId = "<subscriptionid>"
+    $AzureAplicationId ="<applicationid>"
+    $AzureApplicationSecret = ConvertTo-SecureString "h2l8Q~MaUKa1sDCAofj7WTZGa7JusyYAY7I9Nco9" -AsPlainText -Force
+    $psCred = New-Object System.Management.Automation.PSCredential($azureAplicationId , $AzureApplicationSecret)
+    Connect-AzAccount -ServicePrincipal -Credential $psCred -Tenant $azureTenantId -Subscription $AzureSubscriptionId
     # Get the current time
     $currentTime = Get-Date
-    # Set the Routing weight of secondary connection to 30 and primary connection to 20
+    # Set the Routing weight of secondary connection to 100 and primary connection to 0
     Set-AzExpressRouteConnection -Name $PrimaryERConnectionName -ExpressRouteGatewayName $ExpressRouteGatewayName -RoutingWeight 20
     Set-AzExpressRouteConnection -Name $SecondaryERConnectionName -ExpressRouteGatewayName $ExpressRouteGatewayName -RoutingWeight 30
     Write-Host "$($currentTime): Ping to $OnpremIpAddress failed. Set the Routing weight of secondary connection to 30 and primary connection to 20"
